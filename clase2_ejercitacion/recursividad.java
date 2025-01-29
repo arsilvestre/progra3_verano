e9a18131b4e4e85984552f448e516270317c84ae
arsilvestre <arsilvestre@uade.edu.ar>
2025-01-29 16:49:45 -0300

    recursividad02

--- clase2_ejercitacion/recursividad.java
+++ clase2_ejercitacion/recursividad.java
@@ -1,5 +1,17 @@
-class Main {
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA");
+        int numero = 5;
+        System.out.println("El factorial de " + numero + " es: " + factorial(numero));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
 }
