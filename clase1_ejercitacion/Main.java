7f42fceb12918f80d96a90a3c2eaed96e4b3158d
arsilvestre <arsilvestre@uade.edu.ar>
2025-01-29 11:58:16 -0300

    ejercicio1

---
+++ clase1_ejercitacion/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int edad = 38;
+        double altura = 1.75;
+        char inicial = 'a';
+        String ciudad = "Berazategui";
+        int edadAumentada = edad+2;
+        double alturaMultiplicada = altura*2;
+        
+        
+        System.out.println("1_ " + edadAumentada);
+        System.out.println("2_ " + alturaMultiplicada + " mts");
+        System.out.println("3_ " + "mi inicial es: " + inicial);
+        
+	}
+}
